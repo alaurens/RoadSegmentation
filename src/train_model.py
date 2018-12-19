5f5c54d2f1683130e589127c8c90ace924e43cf0
alaurens <laurens_antoine@hotmail.fr>
2018-12-19 09:50:03 +0100

    made train model more general

--- src/train_model.py
+++ src/train_model.py
@@ -7,49 +7,59 @@ import csv
 from logs_process import log_info
 
 
-iter = 240
-epoch_step = 10
-steps_per_epoch = 300
+iter = 276
+epoch_step = 1
+steps_per_epoch = 1
 
 if not os.path.exists(LOGS_PATH):
     os.mkdir(LOGS_PATH)
 
-layers_size = [64, 128, 256, 512]
 
-for in_size in [200, 400]:
+model_num = 1
+layers = [128, 256, 512, 1024]
+in_sizes = [400]
+
+#model_num = 2
+#layers = [64]*5
+#in_sizes = [160, 320]
+
+
+activation = 'leaky'
+
+for in_size in in_sizes:
     train_data_gen = train_generator(in_size)
     validation_data_gen = validation_generator(in_size)
 
-    for i in [1, 2]:
-        layers = list(map(lambda x: i*x, layers_size))
+    if model_num == 1:
+        model = unet1(input_size=(in_size, in_size, 3), layers=layers, activation=activation)
+    else:
+        model = unet2(input_size=(in_size, in_size, 3), layers=layers, activation=activation)
 
-        model = unet(input_size=(in_size, in_size, 3), layers=layers)
+    Learning_reduction = ReduceLROnPlateau(monitor='val_acc', factor=0.3, patience=10,
+                                           verbose=1, mode='auto',
+                                           min_delta=0.0001, cooldown=0, min_lr=0)
+    Early_Stopping = EarlyStopping(monitor='val_acc', min_delta=0.00001, patience=10,
+                                   verbose=1, mode='auto', baseline=None,
+                                   restore_best_weights=False)
+    last_epoch = 0
+    for epochs in range(1, 10):
 
-        Learning_reduction = ReduceLROnPlateau(monitor='val_acc', factor=0.3, patience=10,
-                                               verbose=1, mode='auto',
-                                               min_delta=0.0001, cooldown=0, min_lr=0)
-        Early_Stopping = EarlyStopping(monitor='val_acc', min_delta=0.00001, patience=10,
-                                       verbose=1, mode='auto', baseline=None,
-                                       restore_best_weights=False)
-        last_epoch = 0
-        for epochs in range(1, 10):
+        iter = iter+1
+        init_epoch = last_epoch
+        last_epoch = epoch_step*epochs
 
-            iter = iter+1
-            init_epoch = last_epoch
-            last_epoch = epoch_step*epochs
+        history = History()
+        model_checkpoint = ModelCheckpoint(LOGS_PATH + '/weights' + str(iter) + '.hdf5',
+                                           monitor='val_acc', verbose=1, save_best_only=True)
 
-            history = History()
-            model_checkpoint = ModelCheckpoint(LOGS_PATH + '/weights' + str(iter) + '.hdf5',
-                                               monitor='val_acc', verbose=1, save_best_only=True)
+        model.fit_generator(train_data_gen, steps_per_epoch=steps_per_epoch,
+                            epochs=last_epoch, initial_epoch=init_epoch,
+                            callbacks=[Learning_reduction, history,
+                                       Early_Stopping, model_checkpoint],
+                            validation_steps=20, validation_data=validation_data_gen,
+                            use_multiprocessing=True)
 
-            model.fit_generator(train_data_gen, steps_per_epoch=steps_per_epoch,
-                                epochs=last_epoch, initial_epoch=init_epoch,
-                                callbacks=[Learning_reduction, history,
-                                           Early_Stopping, model_checkpoint],
-                                validation_steps=20, validation_data=validation_data_gen,
-                                use_multiprocessing=True)
-
-            hist = history.history
-            log_info(iter, in_size, layers, last_epoch, steps_per_epoch,
-                     hist['acc'], hist['val_acc'],
-                     hist['loss'], hist['val_loss'])
+        hist = history.history
+        log_info(iter, in_size, layers, last_epoch, steps_per_epoch,
+                 hist['acc'], hist['val_acc'],
+                 hist['loss'], hist['val_loss'], activation)
