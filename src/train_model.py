cd0f537d84d1b770d9f5d337ea1b742e16ed9465
alaurens <laurens_antoine@hotmail.fr>
2018-12-19 12:13:37 +0100

    changed model

--- src/train_model.py
+++ src/train_model.py
@@ -8,7 +8,7 @@ from logs_process import log_info
 
 
 iter = 286
-relabel_mask = False
+relabel_mask = True
 epoch_step = 10
 steps_per_epoch = 300
 num_epoch = 100
@@ -17,13 +17,13 @@ if not os.path.exists(LOGS_PATH):
     os.mkdir(LOGS_PATH)
 
 
-model_num = 1
-layers = [128, 256, 512, 1024]
-in_sizes = [400]
+#model_num = 1
+#layers = [128, 256, 512, 1024]
+#in_sizes = [400]
 
-#model_num = 2
-#layers = [64]*5
-#in_sizes = [160, 320]
+model_num = 2
+layers = [64]*5
+in_sizes = [160, 320]
 
 
 activation = 'relu'
